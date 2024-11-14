bfc40db04aec09cd7931235230f94e8fdb4bfc6b
thuavanan2004 <thuavanan2004@gmail.com>
2024-11-14 16:46:38 +0700

    fix post image use cloundinary

--- routes/admin/account.routes.js
+++ routes/admin/account.routes.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const uploadCloudinaryMiddleware = require("../../middlewares/admin/uploadCloud.middleware");
+
+
+const upload = multer();
 
 const {
   checkPermission
@@ -9,9 +14,9 @@ const controllers = require("../../controllers/admin/account.controller");
 
 router.get("/get-all-account", checkPermission("READ_ADMIN"), controllers.getAll);
 
-router.post("/create", checkPermission("CREATE_ADMIN"), controllers.create);
+router.post("/create", upload.single("avatar"), uploadCloudinaryMiddleware.uploadSingle, checkPermission("CREATE_ADMIN"), controllers.create);
 
-router.patch("/update/:adminId", checkPermission("UPDATE_ADMIN"), controllers.update);
+router.patch("/update/:adminId", upload.single("avatar"), uploadCloudinaryMiddleware.uploadSingle, checkPermission("UPDATE_ADMIN"), controllers.update);
 
 router.patch("/remove/:adminId", checkPermission("DELETE_ADMIN"), controllers.remove);
 
