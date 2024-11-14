4f018b887809006b4af56b8c041cfd9dd4fc854b
thuavanan2004 <thuavanan2004@gmail.com>
2024-11-14 20:35:47 +0700

    fix

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const port = process.env.PORT;
 app.use((req, res, next) => {
   // Thêm header CORS cho mọi yêu cầu
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Cho phép yêu cầu từ localhost:5173
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Các phương thức HTTP được phép
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH'); // Các phương thức HTTP được phép
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With'); // Các header được phép
   res.header('Access-Control-Allow-Credentials', 'true'); // Nếu bạn cần gửi cookie hoặc thông tin xác thực
 
